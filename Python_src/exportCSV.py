447713d1d65855247aecebfa54dd33eb1bda8ff3
EllisBarnwell <ellis.barnwell@gmail.com>
2021-01-02 11:29:28 +0000

    Added basic CSV exporter. Could be a lot more clever with a bit of work

---
+++ Python_src/exportCSV.py
@@ -0,0 +1,14 @@
+import DBAccess
+import configparser
+import pandas as pd
+
+config = configparser.ConfigParser()
+config.read("Radiator_temp_logger.cnf")
+
+databasePath = config["DatabaseSettings"].get("databasePath")
+dbConnection = DBAccess.create_connection(databasePath)
+
+allData_DF = DBAccess.getAllData_DF(dbConnection)
+
+# Export allData_DF as csv
+allData_DF.to_csv ("export.csv", index = False, header=True)
